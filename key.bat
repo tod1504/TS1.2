9b0c1f8697b4bcb5bfcbd8b6d266ec71cb07b9bf
Oscar Lopez <43121128+tod1504@users.noreply.github.com>
2021-10-22 19:46:27 -0700

    Update key.bat

--- key.bat
+++ key.bat
@@ -1,9 +1,9 @@
 @echo off
-set/p x = Ingrese numero
+set/p x = Ingrese numero 
 set cont = 0
 :inicio
 echo %cont%
 set /a cont=cont+1
-if /i "%cont%" equ "%x%" goto final
+if /i "%cont%"=="%x%" goto final
 goto inicio
 :final
