699752832aaa9de7f7311f17e03efcdfb8da5cd3
Oscar <keyle0015@gmail.com>
2021-10-22 22:18:39 -0700

    Lopez C Oscar Andres

--- key.bat
+++ key.bat
@@ -1,9 +1,10 @@
 @echo off
-set/p x = Ingrese numero
+set/p x = Ingrese numero 
 set cont = 0
 :inicio
-echo %cont%
+echo mejorar en batch
 set /a cont=cont+1
 if %cont% == %x% goto :final
 goto inicio
 :final
+@pause 
